44b106c17a6e140ac692849721c5de1b18ebb4dc
stlouisweb <stlouisweb@gmail.com>
2016-03-19 10:27:23 -0500

    coming soon

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -1,37 +1,6 @@
 import React from 'react';
 
-toggleMenu: function() {
-  console.log(this);
-}
 export default () =>
-  <div className="jm_header container-fluid">
-    <nav className="jm_main-nav navbar navbar-inverse navbar-fixed-top">
-      <div className="container-fluid">
-        <div className="navbar-header">
-          <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-            <span className="sr-only">Toggle navigation</span>
-            <span className="icon-bar"></span>
-            <span className="icon-bar"></span>
-            <span className="icon-bar"></span>
-          </button>
-          <a className="jm_logo navbar-brand" href="#">John Marshall<span id="tagline"> &mdash; Georgia Golf Instructor</span></a>
-        </div>
-        <div id="navbar" className="collapse navbar-collapse navbar-right">
-          <ul className="nav navbar-nav">
-            <li><a href="#"><i className="fa fa-phone"></i> (404) 405-1403</a></li>
-            <li className="dropdown"><a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false" onClick={this.toggleMenu}>Menu <i className="fa fa-bars"></i></a>
-              <ul className="dropdown-menu">
-                <li><a href="#">Action</a></li>
-                <li><a href="#">Another action</a></li>
-                <li><a href="#">Something else here</a></li>
-                <li role="separator" className="divider"></li>
-                <li><a href="#">Separated link</a></li>
-                <li role="separator" className="divider"></li>
-                <li><a href="#">One more separated link</a></li>
-              </ul>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
+  <div className="container-fluid">
+    <h1>Saint Louis Web Services</h1>
   </div>;
--- app/components/Home.jsx
+++ app/components/Home.jsx
@@ -1,20 +1,9 @@
 import React from 'react';
 
 export default () =>
-  <div className="jm_main-content container-fluid">
-    <div className="jumbotron">
-      <h1>Learn how to swing from a <br></br>
-        <span id="header-copy-2">Long-Drive Champion!</span>
-      </h1>
-      <blockquote>
-        <p><i className="fa fa-quote-left"></i> John Marshall is quite simply the best teaching professional I have ever been privileged to work with. <i className="fa fa-quote-right"></i></p>
-        <footer><b>Tom Slagle - </b><cite title="location">Knoxville, TN</cite></footer>
-      </blockquote>
+  <div className="home">
+    <div className="main">
     </div>
-    <div className="center-block home-btn-nav">
-       <button type="button" className="btn btn-defalul btn-trans btn-lg">Lessons</button>
-       <button type="button" className="btn btn-default btn-trans btn-lg">Pro Tips</button>
-       <button type="button" className="btn btn-default btn-trans btn-lg">Subscribe</button>
-    </div>
-
+    <p>Website coming soon..</p>
+    <p>Current clients access your accounts <a href="http://store.stlouiswebservices.com">here</a></p>
   </div>;
